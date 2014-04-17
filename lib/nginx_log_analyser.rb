3bbac24f6bd668b77f88138acbec070b3ee15844
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-04-17 17:51:06 +0100

    adding new test sessions and snippets

--- lib/nginx_log_analyser.rb
+++ lib/nginx_log_analyser.rb
@@ -1,3 +1,4 @@
+require 'cgi'
 require 'json'
 require 'csv'
 require_relative 'tsung_wrapper'
@@ -7,11 +8,11 @@ class NginxLogAnalyser
 	def initialize
 		@log = File.new(File.join(TsungWrapper.root, 'log', 'nginx.log'), 'r')
 		@headers = %w{ timestamp remote_addr remote_user body_bytes_sent request_time status request request_method }
-		@fields = %w{ remote_addr remote_user body_bytes_sent request_time status request request_method }
+		@fields = %w{ remote_addr remote_user body_bytes_sent request_time status request request_method params}
 	end
 
 	def run
-		CSV.open(File.join(TsungWrapper.root, 'log', 'ngninx.csv'), 'w') do |csv|
+		CSV.open(File.join(TsungWrapper.root, 'log', 'nginx.csv'), 'w') do |csv|
 			csv << @headers
 			while !@log.eof? do
 				line = @log.readline
@@ -22,6 +23,10 @@ class NginxLogAnalyser
 				@fields.each do |field|
 					array << fields[field]
 				end
+				params = hash['@request']
+
+
+				array << CGI.unescape(params)
 				csv << array
 			end
 		end
