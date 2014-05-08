1de4c7636c2b3a7610574742b11864ae79876bf4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-05-08 14:03:31 +0100

    WIP

--- lib/dump_file_url_analyser.rb
+++ lib/dump_file_url_analyser.rb
@@ -1,15 +1,16 @@
 require 'csv'
+require 'ostruct'
 
 module TsungWrapper
 
   class DumpFileUrlAnalyser
 
     def initialize(filename)
-      @filename   = filename
-      @ifile      = File.open(@filename, 'r')
-      @urls       = Hash.new(Array.new)
-      @start_time = nil
-
+      @filename       = filename
+      @ifile          = File.open(@filename, 'r')
+      @urls           = Hash.new(Array.new)
+      @start_time     = nil
+      @response_codes = []
       check_header_line
     end
 
@@ -33,36 +34,51 @@ module TsungWrapper
       header += @response_codes
       CSV.open(format_ofile_name(@filename), "wb") do |csv|
         csv << header
-        @urls.each { |url, ostruct_array| csv << format_output_for_url(url, ostruct_array) }
+        @urls.keys.sort.each do |url|
+          csv << format_output_for_url(url, @urls[url])
+        end
       end
     end
+
+
 
 
     # iterate through the array of structs for this url, summarising the totals for ervery url and outputting to CSV
     def format_output_for_url(url, ostruct_array)
       # url num_requests avg min max max_elapsed 
-      response_codes = Hash.new(0)
+      response_code_counts = intantiate_empty_response_codes_count
       count = 0
       total_duration = 0
       min_duration = 9999999999
       max_duration = 0
       max_elapsed = 0
       ostruct_array.each do |ostruct|
-        count           == 1
+        count           += 1
         total_duration += ostruct.duration
         min_duration    = ostruct.duration if ostruct.duration < min_duration
-        max_duration    = ostruct.duration if ostruct.duration > max_duration
-        max_elapsed     = ostruct.duration if ostruct.duration > max_duration
+        if ostruct.duration > max_duration
+          max_duration    = ostruct.duration 
+          max_elapsed     = ostruct.esecs 
+        end
+        response_code_counts[ostruct.response_status] += 1
       end
+      
       arr = [url, count, (total_duration.to_f / count).round(2), min_duration, max_duration, max_elapsed]
+      arr += response_code_counts.values
     end
 
+
+    def intantiate_empty_response_codes_count
+      hash = Hash.new
+      @response_codes.each { |code| hash[code] = 0 }
+      hash
+    end
 
 
 
     def response_codes
       codes = []
-      @urls.each do |arr|
+      @urls.each do |url, arr|
         codes << arr.map(&:response_status)
       end
       codes.flatten.uniq.sort
@@ -97,7 +113,11 @@ module TsungWrapper
       url.esecs           = calculate_elapsed_time(fields.first)
       url.duration        = fields[8].to_f
       url.response_status = fields[6].to_s
-      @urls[normalised_url] << url
+      if @urls.has_key?(normalised_url)
+        @urls[normalised_url] << url
+      else
+        @urls[normalised_url] = [ url ]
+      end
     end
 
     def calculate_elapsed_time(field)
@@ -126,9 +146,13 @@ module TsungWrapper
       else
         normalised_url = url
       end
-      normalised_url = normalised_url.nil? ? '' : normalised_url
-      normalised_url.sub(/\/$/,'')             # remove trailing slash if present
+      normalised_url.sub!(/\/$/,'')                      # remove trailing slash if present
+      normalised_url = '/' if normalised_url.nil?         # jsut slash if nil
+      normalised_url = '/' if normalised_url == ''        # just slash if empty
+      normalised_url
     end
+
+
 
 
   end
--- spec/models/dump_file_url_analyser_spec.rb
+++ spec/models/dump_file_url_analyser_spec.rb
@@ -9,8 +9,8 @@ module TsungWrapper
     describe 'private method normalise_url' do
       it 'should normalise as expected' do
         dfua.send(:normalise_url, '/activate/12254577').should == '/activate'
-        dfua.send(:normalise_url, '/?autoTestKey=abdjskkdhjfhf').should == ''
-        dfua.send(:normalise_url, '/').should == ''
+        dfua.send(:normalise_url, '/?autoTestKey=abdjskkdhjfhf').should == '/'
+        dfua.send(:normalise_url, '/').should == '/'
         dfua.send(:normalise_url, '/create/donor/').should == '/create/donor'
         dfua.send(:normalise_url, '/create/donor/?add=kkdj').should == '/create/donor'
         dfua.send(:normalise_url, '/create/donor?add=kkdj').should == '/create/donor'
@@ -21,13 +21,28 @@ module TsungWrapper
     describe '#run' do
       it 'should analyse the file and produce the expected results' do
         dfua.run
-        fp = File.new(File.join(TsungWrapper.config_dir, 'data', 'tsung.dump_urls.csv'))
-        puts fp.read
+        fp = File.open(File.join(TsungWrapper.config_dir, 'data', 'tsung.dump_urls.csv'))
+        actual = fp.read
         fp.close
+
+        actual.should == expected_CSV_ouput
         
       end
     end
 
   end
 
+end
+
+
+
+
+def expected_CSV_ouput
+  str = <<-EOS
+url,num_requests,avg,min,max,max_elapsed,200,302,402,500,502
+/,13,196.81,179.692,238.42,0,13,0,0,0,0
+/activate,4,188.5,177.228,198.338,9,2,1,1,0,0
+/add/donor,5,198.66,187.255,220.488,7,5,0,0,0,0
+/create/donor,6,200.98,188.18,232.449,14,4,0,0,1,1
+EOS
 end
